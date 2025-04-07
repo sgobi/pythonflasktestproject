7465a42df27bd90586e2969c7bd775263236c18b
Gobikrishna <g2k2@live.com>
2025-04-07 15:18:04 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-#pip install flask 
+never wash #pip install flask 
 
 from flask import Flask  #This imports Flask, a lightweight web framework for Python
 
@@ -7,7 +7,7 @@ app = Flask(__name__)    #Creates an instance of the Flask application.
 
 @app.route('/')       #@app.route('/'): Defines a route (URL path)
 def home():
-    return "Hello, Jaffna  Small Kids! when ever say I'm pavam you know!!"
+    return "Hello, Jaffna  Small Kids! when ever say I'm pavam you know   he never wash his cloths!!"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)   #debug=True → Enables auto-restart on code changes and provides detailed error logs.
