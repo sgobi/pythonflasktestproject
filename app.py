5b26237b0fe02a2704783f3ee87a02f9489e5a46
Gobikrishna <g2k2@live.com>
2025-04-07 12:16:57 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,13 @@
+#pip install flask 
+
+from flask import Flask  #This imports Flask, a lightweight web framework for Python
+
+app = Flask(__name__)    #Creates an instance of the Flask application. 
+                        #__name__ tells Flask whether the script is run directly or imported.
+
+@app.route('/')       #@app.route('/'): Defines a route (URL path)
+def home():
+    return "Hello, Small Kids!"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)   #debug=True → Enables auto-restart on code changes and provides detailed error logs.
